1231bc11570195feb6e31cd3ded42aeaeb305944
AlekseyLeshko <alekseyleshko@gmail.com>
2016-11-14 01:39:36 +0400

    update currency reducers, use currencies module

--- src/currency-reducers.js
+++ src/currency-reducers.js
@@ -1,20 +1,13 @@
-const currencyReducers = {
-  rubles: (state, item) => {
-    return state.rubles += item.price / 0.7;
-  },
-  dollars: (state, item) => {
-    return state.dollars += item.price / 1;
-  },
-  euros: (state, item) => {
-    return state.euros += item.price / 1.1;
-  },
-  yens: (state, item) => {
-    return state.yens += item.price / 1.2;
-  },
-  pounds: (state, item) => {
-    return state.pounds += item.price / 1.3;
-  }
-};
+import currencies from './currencies';
+import exchangeRate from './exchange-rate';
 
-export default currencyReducers;
+const curr = Object.keys(currencies).reduce((obj, currency) => {
+  obj[currency] = (state, item) => {
+    return state[currency] += item.price / exchangeRate[currency];
+  };
+
+  return obj;
+}, {});
+
+export default curr;
 
