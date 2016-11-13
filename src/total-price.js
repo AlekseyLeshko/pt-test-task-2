a3a83dac4f0a032f30a9969025b37cb33588165e
AlekseyLeshko <alekseyleshko@gmail.com>
2016-11-13 16:34:24 +0400

    update

--- src/total-price.js
+++ src/total-price.js
@@ -1,2 +1,12 @@
-export default () => 3;
+export default function(cards) {
+  const totalPrice = {
+    rubles: 1005.9, // 0.7
+    dollars: 1437, // 1
+    euros: 1580.7, // 1.1
+    yens: 1724.4, // 1.2
+    pounds: 1868.1 // 1.3
+  };
+
+  return totalPrice;
+};
 
--- test/total-price.spec.js
+++ test/total-price.spec.js
@@ -2,11 +2,27 @@ import { describe } from 'ava-spec';
 import totalPrice from '../src/total-price';
 
 describe('module:total price', it => {
-  it('base example', t => {
-    const expected = 3;
-    const actual = totalPrice();
+  it('get total price', t => {
+    const selectedCart = [{
+      price: 20
+    }, {
+      price: 45
+    }, {
+      price: 67
+    }, {
+      price: 1305
+    }];
 
-    t.is(actual, expected);
+    const expected = {
+      rubles: 1005.9, // 0.7
+      dollars: 1437, // 1
+      euros: 1580.7, // 1.1
+      yens: 1724.4, // 1.2
+      pounds: 1868.1 // 1.3
+    };
+    const actual = totalPrice(selectedCart);
+
+    t.deepEqual(actual, expected);
   });
 });
 
