86ae00e4d5110b827ad4bd1dc97ef9d85f5c9c7a
AlekseyLeshko <alekseyleshko@gmail.com>
2016-11-13 17:58:15 +0400

    fix

--- src/currency-reducers.js
+++ src/currency-reducers.js
@@ -1,17 +1,17 @@
 const currencyReducers = {
-  rubles: function(state, item) {
+  rubles: (state, item) => {
     return state.rubles += item.price / 0.7;
   },
-  dollars: function(state, item) {
+  dollars: (state, item) => {
     return state.dollars += item.price / 1;
   },
-  euros: function(state, item) {
+  euros: (state, item) => {
     return state.euros += item.price / 1.1;
   },
-  yens: function(state, item) {
+  yens: (state, item) => {
     return state.yens += item.price / 1.2;
   },
-  pounds: function(state, item) {
+  pounds: (state, item) => {
     return state.pounds += item.price / 1.3;
   }
 };
